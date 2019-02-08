b4b19898989e649e21e968cee633861df63cac52
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-08 14:26:10 -0800

    starting luhn checker project

---
+++ luhn.js
@@ -0,0 +1,19 @@
+exports.check = (num) => {
+  return checkLuhn(num);
+}
+
+// some function to check validity of passed number
+function checkLuhn(num) {
+
+}
+
+// From the rightmost digit, which is the check digit, and moving left, double the value of every 
+// second digit. The check digit is not doubled; the first digit doubled is immediately to the left 
+// of the check digit. If the result of this doubling operation is greater than 9 (e.g., 8 × 2 = 16), 
+// then add the digits of the result (e.g., 16: 1 + 6 = 7, 18: 1 + 8 = 9) or, alternatively, the 
+// same final result can be found by subtracting 9 from that result (e.g., 16: 16 − 9 = 7, 18: 18 − 9 = 9).
+
+// Take the sum of all the digits.
+
+// If the total modulo 10 is equal to 0 (if the total ends in zero) then the number is valid 
+// according to the Luhn formula; else it is not valid.
